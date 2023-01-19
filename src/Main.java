e793e5a6e8f56145422250311a111f604766231e
enovak89 <enovak89@mail.ru>
2023-01-19 07:55:04 +0300

    task 1-10

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,94 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
+        task9();
+        task10();
     }
 
     public static void task1() {
         System.out.println("Задача 1");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
 
+    public static void task2() {
+        System.out.println("Задача 2");
+        for (int i = 10; i > 0; i -= 1) {
+            System.out.println(i);
+        }
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3");
+        for (int i = 0; i < 17; i += 1) {
+            System.out.print((i % 2 == 0 && i != 0) ? i + "\n" : "");
+        }
+        for (int i = 2; i < 17; i += 2) {
+            System.out.println(i);
+        }
+    }
+
+    public static void task4() {
+        System.out.println("Задача 4");
+        for (int i = 10; i > -11; i--) {
+            System.out.println(i);
+        }
+    }
+
+    public static void task5() {
+        System.out.println("Задача 5");
+        for (int i = 1904; i <= 2096; i += 4) {
+            System.out.printf("%d год является високосным\n", i);
+        }
+    }
+
+    public static void  task6() {
+        System.out.println("Задача 6");
+        for (int i = 7; i <= 98; i += 7) {
+            System.out.println(i);
+        }
+    }
+
+    public static void  task7() {
+        System.out.println("Задача 7");
+        for (int i = 1; i <= 512; i *= 2) {
+            System.out.println(i);
+        }
+    }
+
+    public static void  task8() {
+        System.out.println("Задача 8");
+        int savins = 29;
+        int totalSavings = 0;
+        for (int i = 1; i <= 12; i += 1) {
+            totalSavings += savins;
+            System.out.printf("За %d месяц сумма накоплений равна %d\n", i, totalSavings);
+        }
+    }
+
+        public static void  task9() {
+            System.out.println("Задача 9");
+            int savins = 29;
+            float totalSavings = 0f;
+            for (int i = 1; i <= 12; i += 1) {
+                totalSavings += totalSavings / 100;
+                totalSavings += savins;
+                System.out.printf("За %d месяц сумма накоплений равна %.2f\n", i, totalSavings);
+            }
+        }
+
+    public static void  task10() {
+        System.out.println("Задача 10");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("2 * " + i + " = " + i * 2);
+        }
     }
 }
